7b29c5c38634bcc223ef964a8c36e898a5a900ad
Ramaz Artmeladze <ramaz.artmeladze@outlook.com>
2024-01-06 13:10:28 +0400

    Update

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -3,6 +3,6 @@ package com.epam.utils;
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
          //here magic will happen
-        return true;
+        return StringUtils.isNumeric(str) && Integer.parseInt(str) > 0;
     }
 }
