fc738dee9e5715d6dd553ffd79359b6c66587ba6
Ramaz Artmeladze <ramaz.artmeladze@outlook.com>
2024-01-06 12:11:44 +0400

    Update

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -4,6 +4,16 @@ import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        //magic happens here
+        for (String arg : args) {
+            try {
+                int number = Integer.parseInt(arg);
+                if (number <= 0) {
+                    return false;
+                }
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        }
+        return true;
     }
 }
